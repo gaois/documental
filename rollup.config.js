3722f2d2ee1ea4c8c9e157152c776bf41679f917
Ronan Doherty <ronan.odochartaigh@gmail.com>
2020-01-18 18:54:19 +0000

    Leathanach earráide

--- rollup.config.js
+++ rollup.config.js
@@ -46,7 +46,6 @@ export default {
 			}),
 			commonjs(),
 			json(),
-
 			legacy && babel({
 				extensions: ['.js', '.mjs', '.html', '.svelte'],
 				runtimeHelpers: true,
--- src/routes/_error.svelte
+++ src/routes/_error.svelte
@@ -1,11 +1,37 @@
 <script>
+	import settings from 'settings';
+    import { _ } from 'svelte-i18n';
+
 	export let status;
 	export let error;
 
 	const dev = process.env.NODE_ENV === 'development';
 </script>
 
+<svelte:head>
+	<title>{status}</title>
+	<meta name='description' content={error.message}>
+</svelte:head>
+
+<div class='message'>
+	<h1>{status}</h1>
+	{#if (status === 404)}
+		<p>{$_('error.pageNotFound')}.</p>
+	{:else}
+		<p>{error.message}</p>
+	{/if}
+	{#if dev && error.stack}
+		<pre>{error.stack}</pre>
+	{/if}
+	<hr/>
+	<p>{$_('error.contact')}: <a href={`mailto:${settings.siteEmail}`}>{settings.siteEmail}</a>.</p>
+</div>
+
 <style>
+	.message {
+		margin-top: 1.6rem;
+	}
+
 	h1, p {
 		margin: 0 auto;
 	}
@@ -19,22 +45,27 @@
 	p {
 		margin: 1em auto;
 	}
+	
+	pre {
+		overflow: auto;
+		width: 100%;
+	}
+
+	hr {
+		border-color: #ccc;
+		color: #ccc;
+	}
 
 	@media (min-width: 480px) {
 		h1 {
 			font-size: 4em;
 		}
 	}
+
+	@media (min-width: 1000px) {	
+		.message {
+			margin-top: 6rem;
+			padding: 0 2rem;
+		}
+	}
 </style>
-
-<svelte:head>
-	<title>{status}</title>
-</svelte:head>
-
-<h1>{status}</h1>
-
-<p>{error.message}</p>
-
-{#if dev && error.stack}
-	<pre>{error.stack}</pre>
-{/if}
